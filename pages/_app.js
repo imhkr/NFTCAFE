c2ab3235daaaa5475bffb85033cb4f88becaceec
imhkr <himanshu.patel_cs19@gla.ac.in>
2022-12-05 07:05:57 +0530

    Basic Setup Done

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/47485decec.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+export default MyApp;
